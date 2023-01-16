e3721a7fd14e1cb3c8e68ae313ba76c3512c9381
ItZRockyboi <112095621+ItZRockyboi@users.noreply.github.com>
2023-01-16 17:44:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup(){
+canvas = createCanvas(300,300);
+canvas.center();
+}
+
+
+function draw(){
+
+}
+
+
+function preload(){
+
+}
+
+
+function takesnapshot(){
+save("HimanshuVyas.png");
+}
